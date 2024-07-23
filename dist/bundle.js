dc01fa2e370f2ae4bea7e2c5c3ce6976b18c5991
andremarinhodev <79175971+andremarinhodev@users.noreply.github.com>
2024-07-23 02:44:33 -0300

    removed the now ignored directory dist

--- dist/bundle.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function App() {
-  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
-}
